e19af357f6d681ad709565a5cf17d3e72ad4fc88
Tusori Tibor <tusori.tibor94@gmail.com>
2026-02-25 18:13:05 +0100

    fix: Prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,7 +11,10 @@ function createPrismaClient(): PrismaClient {
   if (!connectionString) {
     throw new Error("DATABASE_URL environment variable is not set");
   }
-  const pool = new pg.Pool({ connectionString });
+  const pool = new pg.Pool({
+    connectionString,
+    ssl: { rejectUnauthorized: false },
+  });
   const adapter = new PrismaPg(pool);
   return new PrismaClient({ adapter });
 }
